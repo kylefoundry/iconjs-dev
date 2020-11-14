c934692cbe873bcbac3442d08daa6378144f6f6d
Dennis Dang <dangggdennis@gmail.com>
2020-11-14 15:51:47 -0500

    use the new Iconjs API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import "./App.css";
-import Icon from "iconjs";
+import Icon, { Plus, IconjsProvider, setGlobalIconJs } from "iconjs";
 
 function App() {
-  return (
-    <div className="App">
-      <Icon icon="plus" />
-      <Icon icon="credit-card" />
-      <Icon
-        icon="credit-card"
-        color="#000"
-        fill="#8800ff"
-        accent="#000"
-        accent2="#00ff00"
-        stroke="2"
-      />
-      <Icon icon="credit-card" fill="#000" stroke="0" />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Icon icon={Plus} fill="#b3b3b3" stroke="6" />
+			{/* <Icon
+				icon={CreditCard}
+				color="#000"
+				fill="#8800ff"
+				accent="#000"
+				accent2="#00ff00"
+				stroke="2"
+			/> */}
+			<Icon icon={Plus} />
+			{/* <Icon icon={CreditCard} /> */}
+		</div>
+	);
 }
 
-export default App;
+function AppProvider() {
+	// Acts as our global config
+	setGlobalIconJs({
+		color: "#000",
+		fill: "#ccc",
+		accent: "#777",
+		accent2: "#333",
+		stroke: "2",
+		end: undefined,
+	});
+
+	return (
+		<IconjsProvider>
+			<App></App>
+		</IconjsProvider>
+	);
+}
+
+export default AppProvider;
